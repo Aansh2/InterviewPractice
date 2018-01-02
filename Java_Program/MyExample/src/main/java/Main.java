4964929bfb18e5f7f9f2948704653d2727820e23
Nikhil Soni <nikhil.soni@harbingergroup.com>
2018-01-02 22:53:07 +0530

    Fisrt Commit

---
+++ Java_Program/MyExample/src/main/java/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by nikhil on 11/9/16.
+ */
+import javax.mail.*;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+public class Main {
+
+public static void main(String [] args) {
+
+}
+
+}
