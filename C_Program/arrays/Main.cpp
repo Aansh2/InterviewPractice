1e9e1e3ab1db89056c0865b4bee74493d62b73cc
Nikhil Soni <nikhil.soni@harbingergroup.com>
2018-01-02 23:50:45 +0530

    upated

---
+++ C_Program/arrays/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// using namespace std;
+
+// create array
+void createArray(){
+    // Array declaration and initialization
+    int arr[] = {1,2,3,4,5,6};
+
+    for(int i=0; i< sizeof(arr)/sizeof(arr[0]) ;i++ ){
+        std::cout << "array value of index " << i << ": "<< arr[i] <<"\n";
+    }
+
+}
+int main() {
+   //cout << "Hello, World!" << std::endl;
+      createArray();
+    return 0;
+}
+
