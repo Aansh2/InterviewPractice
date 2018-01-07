8eb561f6f3e4186961d4b11e2065189952a81635
Nikhil Soni <nikhil.soni@harbingergroup.com>
2018-01-07 10:07:26 +0530

    Correction

--- Java_Program/MyExample/src/main/java/Main.java
+++ Java_Program/MyExample/src/main/java/Main.java
@@ -29,26 +29,12 @@ public class Main {
     private void makeMap(){
         Map map = new HashMap<Employee,String>();
         map.put(new Employee("Nikhil",1),"me");
-        map.put(new Employee("Ruby",1),"Nunni");
 
-        map.put(new Employee("Nikhil Soni",1),"again me ");
-
-        map.put(new Employee("Nikhwil",1),"me");
-        map.put(new Employee("Rubwy",1),"Nunni");
-
-        map.put(new Employee("Nikhil Soniw",1),"again me ");
-        map.put(new Employee("Nikhil Sonidw",1),"again me ");
-        map.put(new Employee("Nikhil Sonwiq",1),"again me ");
-        map.put(new Employee("Nikhil Sonqwi",1),"again me ");
-        map.put(new Employee("Nikhil Soddniw",1),"again me ");
-        map.put(new Employee("Nikhil Sofniw",1),"again me ");
-        map.put(new Employee("Nikhil Sognwiq",1),"again me ");
-        map.put(new Employee("Nikhil Sohnqwi",1),"again me ");
         new  IdentityHashMap<String,String>();
         new Hashtable<String,String>();
         LinkedHashMap linkedHashMap =  new LinkedHashMap<Employee,String>();
         linkedHashMap.put(new Employee("Nikhil",1),"me");
-        linkedHashMap.put(new Employee("Ruby",2),"Nunni");
+
 
         linkedHashMap.put(new Employee("Nikhil Soni",1),"again me ");
 
